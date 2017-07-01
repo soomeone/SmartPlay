2b6d2be953888961179360064e6e54a60d963081
soomeone <tipdf@icloud.com>
2017-07-02 00:36:10 +0200

    Added media queue
    - Drop a video to the page and it will be added to the queue
    - Autoplay next media in the queue after one has finished
    - (experimental) smooth transition

--- src/js/script.js
+++ src/js/script.js
@@ -24,6 +24,7 @@ function toggle() {
 }
 
 function move(time) {
+	// Move in the timeline forward and backward
 	player.currentTime += time;
 
 	// Show image
@@ -44,6 +45,7 @@ function setProgress(percent) {
 }
 
 function setTime(seconds) {
+	// Set the current time to a defined number in seconds
 	var m = parseInt(seconds / 60);
 	if (m < 10)
 		m = "0" + m;
@@ -56,12 +58,6 @@ function setTime(seconds) {
 }
 
 /* General functions */
-
-function drop(event) {
-	event.preventDefault();
-	var data = event.dataTransfer.getData("text");
-	event.target.appendChild(document.getElementById(data));
-}
 
 function addMedia(url) {
     document.getElementById("myList").appendChild(node); 
@@ -77,6 +73,43 @@ function fullscreen() {
 
     rfs.call(el);
 }
+
+/* Queue */
+
+var queue = [];
+function addMedia(url) {
+	// Append a new media to the queue
+	queue.push(url);
+	if (queue.length == 1 && player.playing)
+		nextMedia(); // Auto start playing if the queue was empty before
+}
+
+function instantMedia(url) {
+	// Play instantly the selected video and pause the queue (will continue play after this video)
+	queue.unshift(player.src);
+	queue.unshift(url);
+}	
+
+function nextMedia() {
+	if (queue.length >= 1) {
+
+		// Make a cool fadeout effect
+		$("#darkoverlay").fadeIn(1000);
+
+		setTimeout(function() {
+			player.src = queue.shift(); // Get the next video (and remove it from the queue)
+
+			$("#darkoverlay").fadeOut(1000);
+			setTimeout(function() {play();}, 1000);
+		}, 1000);
+	}
+}
+
+player.onended = function(e) {
+      // Automagically continue in the queue when a clip finished
+      pause();
+      nextMedia();
+    };
 
 /* Update showed time and progress */
 setInterval(function(){
@@ -138,3 +171,29 @@ window.onkeydown = function(e) {
    		idletime = 0; // Reset idle timer because a key has been pressed
    	}
 }
+
+
+/* Drag and drop */
+container.addEventListener('dragover', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+});
+
+var URL = window.URL || window.webkitURL;
+
+container.addEventListener('drop', function(e) {
+
+    e.stopPropagation();
+    e.preventDefault();
+    var files = e.dataTransfer.files; // Array of all files
+
+    for (var i=0, file; file=files[i]; i++) { 
+        	var type = file.type;
+        	if (player.canPlayType(type)) {
+        		var fileURL = URL.createObjectURL(file);
+        		addMedia(fileURL);
+        	}
+        }
+    }
+);
